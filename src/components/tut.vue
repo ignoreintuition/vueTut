0b04c69cd00120b028458b795944fad430c94285
Brian Greig <brian@resurgencewebdesign.com>
2017-05-16 22:03:50 -0400

    Initial Commit

---
+++ src/components/tut.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="tut">
+    <h1>Grievances</h1>
+    <ul>
+      <li v-for='grievance in grievances'>
+        {{grievance.text}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'tut',
+  data () {
+    return {
+      grievances: [
+        {text: 'Everyone Hates Me'},
+        {text: 'The World is Against Me'},
+        {text: 'My soul is damned for hell'}
+      ]
+    }
+  }
+}
+</script>
